5070d8b347229e816f8f27c3a5c94f3bd01fc6fc
ArizCefDev <106917027+ArizCefDev@users.noreply.github.com>
2022-09-10 15:04:57 +0400

    Create Error page

---
+++ WebTravel5/Controllers/ErrorController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebTravel5.Controllers
+{
+    [AllowAnonymous]
+    public class ErrorController : Controller
+    {
+        public IActionResult Error1(int code)
+        {
+            return View();
+        }
+    }
+}
