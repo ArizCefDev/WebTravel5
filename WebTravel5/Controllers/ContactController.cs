70b0fc13af0dbc32e14ff0f3e855d18380db3fdb
ArizCefDev <106917027+ArizCefDev@users.noreply.github.com>
2022-09-28 10:40:19 +0400

    Update Project

---
+++ WebTravel5/Controllers/ContactController.cs
@@ -0,0 +1,42 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebTravel5.Controllers
+{
+    [AllowAnonymous]
+    public class ContactController : Controller
+    {
+        private readonly IMessageService _messageService;
+
+        public ContactController(IMessageService messageService)
+        {
+            _messageService = messageService;
+        }
+
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult MessageAdd()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult MessageAdd(Message p)
+        {
+            _messageService.TInsert(p);
+            return RedirectToAction("Index","Contact");
+        }
+
+        public IActionResult Test()
+        {
+            return View();
+        }
+    }
+}
